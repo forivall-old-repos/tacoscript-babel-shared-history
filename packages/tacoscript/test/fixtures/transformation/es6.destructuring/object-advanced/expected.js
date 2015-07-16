d1e4d8b4ecab10a01eef8efc8b595dea1262241d
Jordan Klassen <jordan@klassen.me.uk>
2015-07-16 00:04:56 -0700

    Remove unusable tests

--- packages/tacoscript/test/fixtures/transformation/es6.destructuring/object-advanced/expected.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var rect = {};
-var _rect$topLeft = rect.topLeft;
-var x1 = _rect$topLeft.x;
-var y1 = _rect$topLeft.y;
-var _rect$bottomRight = rect.bottomRight;
-var x2 = _rect$bottomRight.x;
-var y2 = _rect$bottomRight.y;
--- packages/tacoscript/test/fixtures/transformation/es6.spec.symbols/typeof/expected.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var s = Symbol("s");
-assert.equal(typeof s === "undefined" ? "undefined" : babelHelpers._typeof(s), "symbol");
-assert.equal(babelHelpers._typeof(babelHelpers._typeof(s.foo)), "symbol");
